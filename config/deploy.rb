70d976cbff7698ceb23077fd23280539e5a12f71
Dean Chouinard <deanchouinard@gmail.com>
2013-04-12 17:59:15 -0400

    Setting up Capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,60 @@
+require "bundler/capistrano"
+load "deploy/assets"
+
+set :rvm_ruby_string, :local
+set :rvm_autolibs_flag, "read-only"
+
+before 'deploy:setup', 'rvm:install_rvm'
+before 'deploy:setup', 'rvm:install_ruby'
+# before 'deploy:setup', 'rvm:create_gemset'
+
+set :bundle_cmd, 'source $HOME/.bash_profile && bundle'
+
+require "rvm/capistrano"
+
+
+set :application, "Mystore"
+set :repository,  "https://github.com/deanchouinard/gfs.git"
+
+set :scm, :git
+
+# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+server = "localhost"
+role :web, server                          # Your HTTP server, Apache/etc
+role :app, server                          # This may be the same as your `Web` server
+role :db,  server, :primary => true # This is where Rails migrations will run
+
+
+set :user, "spree"
+
+set :deploy_to, "/home/spree/gfs"
+
+set :use_sudo, false
+
+default_run_options[:shell] = '/bin/bash --login'
+
+default_environment["RAILS_ENV"] = 'production'
+
+task :symlink_database_yml do
+  run "rm #{release_path}/config/database.yml"
+  run "ln -sfn #{shared_path}/config/database.yml
+       #{release_path}/config/database.yml"
+end
+after "bundle:install", "symlink_database_yml"
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
