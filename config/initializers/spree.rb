eca8e32994d61ff0e8886865286dc3245117f80b
Dean Chouinard <deanchouinard@gmail.com>
2013-05-23 13:04:12 -0400

    first test and logo change

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -9,6 +9,7 @@ Spree.config do |config|
   # Example:
   # Uncomment to override the default site name.
   # config.site_name = "Spree Demo Site"
+  config.logo = "store/gameface_logo_sm.jpg"
 end
 
 Spree.user_class = "Spree::User"
